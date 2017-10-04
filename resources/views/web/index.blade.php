ff6a058115492cfeb2197867ddaa8827c0e99886
Grzegorz Stefański <grzesiekk94@gmail.com>
2017-10-04 11:40:33 +0200

    fix css

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -16,7 +16,7 @@
     <!-- Custom fonts for this template -->
     <link href="{{url('/plugins')}}/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,900" rel="stylesheet">
 
     <!-- Plugin CSS -->
     <link href="{{url('/plugins')}}/magnific-popup/magnific-popup.css" rel="stylesheet">
