912a4fc55659a011bc50d896022a1510ab1ee57c
Grzegorz Stefański <grzesiekk94@gmail.com>
2017-10-11 14:04:51 +0200

    forgotten changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      mkdir(public_path('blog'), 0755);
-      mkdir(public_path('blog/thumb'), 0755);
-      mkdir(public_path('portfolio'), 0755);
-      mkdir(public_path('portfolio/thumb'), 0755);
+      mkdir(public_path('img/blog'), 0755);
+      mkdir(public_path('img/blog/thumb'), 0755);
+      mkdir(public_path('img/portfolio'), 0755);
+      mkdir(public_path('img/portfolio/thumb'), 0755);
 
       $this->call(AdminSeeder::class);
       $this->command->info("Superadmin seeded!\nlogin:a@a.pl\npasswd:qqq");
